72352a5bddb13b9ab5b8cb7cbf3e5064d57f2747
fpumagonzales <francisco.puma@globant.com>
2022-04-04 19:45:37 -0500

    add number functions

--- main.go
+++ main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+
+	e1 := elements{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	e2 := elements{2, 4, 6, 8, 10}
+
+	sum1 := sumElements(e1)
+	sum2 := sumElements(e2)
+
+	e1.print()
+	fmt.Println("sum1:", sum1)
+
+	e2.print()
+	fmt.Println("sum2:", sum2)
+
 }
---
+++ numbers.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type elements []int
+
+func sumElements(e elements) int {
+	sum := 0
+	for _, item := range e {
+		sum = sum + item
+	}
+
+	return sum
+}
+
+func (e elements) print() {
+	for _, item := range e {
+		fmt.Println(item)
+	}
+
+}
