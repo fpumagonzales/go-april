0d6726211915b48da7e3e267e026b5ff3e78a120
fpumagonzales <francisco.puma@globant.com>
2022-04-04 20:12:21 -0500

    unit test

---
+++ number_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	e1 := elements{1, 3, 5, 7}
+	expectedSum := 16
+	sum1 := sumElements(e1)
+
+	if sum1 != expectedSum {
+		t.Errorf("Expected sum %d, but got ", sum1)
+
+	}
+
+}
