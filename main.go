dedee53ad2593ef87d02a67c2c24d39972d34018
fpumagonzales <francisco.puma@globant.com>
2022-07-21 16:41:38 -0500

    add e3 and sum3

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ func main() {
 
 	e1 := elements{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	e2 := elements{2, 4, 6, 8, 10}
+	e3 := elements{1, 3, 5, 7, 9}
 
 	sum1 := sumElements(e1)
 	sum2 := sumElements(e2)
+	sum3 := sumElements(e3)
 
 	e1.print()
 	fmt.Println("sum1:", sum1)
@@ -16,4 +18,6 @@ func main() {
 	e2.print()
 	fmt.Println("sum2:", sum2)
 
+	e3.print()
+	fmt.Println("sum3:", sum3)
 }
